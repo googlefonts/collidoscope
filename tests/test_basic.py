5d50cad4b473c5d1063ebd9d224af7a640290ea2
Simon Cozens <simon@simon-cozens.org>
2023-02-06 13:37:56 +0000

    Gosh, some tests

---
+++ tests/test_basic.py
@@ -0,0 +1,48 @@
+from collidoscope import Collidoscope
+from pathlib import Path
+from kurbopy import Point
+
+
+DATADIR = Path(__file__).parent / "data"
+
+
+def test_binary():
+    font = DATADIR / "Nunito-Subset.ttf"
+    c = Collidoscope(font, rules={"bases": True})
+    glyphs = c.get_glyphs("aa")
+    assert not c.has_collisions(glyphs)
+    glyphs = c.get_glyphs("ïï")
+    assert c.has_collisions(glyphs)
+
+
+def test_variations():
+    font = DATADIR / "Nunito-Subset.ttf"
+    c = Collidoscope(font, rules={"bases": True})
+    glyphs = c.get_glyphs("Ưï")
+    assert not c.has_collisions(glyphs)
+
+    c = Collidoscope(font, rules={"bases": True}, location={"wght": 1000})
+    glyphs = c.get_glyphs("Ưï")
+    assert c.has_collisions(glyphs)
+
+
+def test_rules():
+    font = DATADIR / "Nunito-Subset.ttf"
+    c = Collidoscope(font, rules={"bases": False})
+    glyphs = c.get_glyphs("ïï")
+    assert not c.has_collisions(glyphs)
+
+    c = Collidoscope(font, rules={"bases": True, "area": 0.05})
+    glyphs = c.get_glyphs("ïï")
+    assert c.has_collisions(glyphs)
+
+    c = Collidoscope(font, rules={"bases": True, "area": 0.5})
+    glyphs = c.get_glyphs("ïï")
+    assert not c.has_collisions(glyphs)
+
+
+# def test_glyphs():
+# 	font = DATADIR / "Nunito.glyphs"
+# 	c = Collidoscope(font, rules = { "bases": True} )
+# 	glyphs = [ c.get_positioned_glyph("a", Point(0, 0)), c.get_positioned_glyph("a", Point(100, 0)) ]
+# 	assert c.has_collisions(glyphs)
